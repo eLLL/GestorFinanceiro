d8356a88c45025184cdb91e67f3127124a6d6bd3
Lucas Didur <lucasdidur@hotmail.com>
2014-11-04 15:15:12 -0200

    Criado Manager

---
+++ src/dlp/control/Manager.java
@@ -0,0 +1,13 @@
+package dlp.control;
+
+import dlp.model.Operacao;
+
+public class Manager
+{
+
+    public static void salvar(Operacao op)
+    {
+        // TODO Auto-generated method stub
+    }
+
+}
