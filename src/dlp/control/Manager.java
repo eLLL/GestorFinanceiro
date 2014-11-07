02e49cf8d3bc782b1084aea8188828e21a42ca68
Lucas Didur <lucasdidur@hotmail.com>
2014-11-07 15:56:37 -0200

    Criado metodo generico salvar para categoria

--- src/dlp/control/Manager.java
+++ src/dlp/control/Manager.java
@@ -1,11 +1,17 @@
 package dlp.control;
 
+import dlp.model.Categoria;
 import dlp.model.Operacao;
 
 public class Manager
 {
 
     public static void salvar(Operacao op)
+    {
+        // TODO Auto-generated method stub
+    }
+    
+    public static void salvar(Categoria op)
     {
         // TODO Auto-generated method stub
     }
