8e5bcbab6984bafcf1732aea0e36363b41629545
Lucas Didur <lucasdidur@hotmail.com>
2014-11-30 00:22:46 -0200

    Corrigido categoria

--- src/dlp/control/AddCategoria.java
+++ src/dlp/control/AddCategoria.java
@@ -9,6 +9,7 @@ import dlp.model.Categoria;
 import dlp.model.Cor;
 import dlp.model.TipoOperacao;
 import dlp.view.CategoriaDespesa;
+import dlp.view.CategoriaReceita;
 import java.awt.event.ActionEvent;
 
 /**
@@ -20,19 +21,19 @@ public class AddCategoria
 
     public static void AddDespesa(ActionEvent evt)
     {
-        add(TipoOperacao.DESPESA);
+        add(CategoriaDespesa.txt_nome.getText(), CategoriaDespesa.slct_cor.getSelectedItem().toString().toUpperCase(), TipoOperacao.DESPESA);
     }
 
     public static void AddReceita(ActionEvent evt)
     {
-        add(TipoOperacao.DESPESA);
+        add(CategoriaReceita.txt_nome.getText(), CategoriaReceita.slct_cor.getSelectedItem().toString().toUpperCase(), TipoOperacao.RECEITA);
     }
 
-    private static void add(TipoOperacao tipo)
+    private static void add(String nome, String cor, TipoOperacao tipo)
     {
         Categoria categoria = new Categoria();
-        categoria.setNome(CategoriaDespesa.txt_nome.getText());
-        categoria.setCor(Cor.valueOf(CategoriaDespesa.slct_cor.getSelectedItem().toString().toUpperCase()));
+        categoria.setNome(nome);
+        categoria.setCor(Cor.valueOf(cor));
         categoria.setTipo(tipo);
 
         categoria.addBD();
