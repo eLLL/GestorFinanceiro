eca31f26ea7b11b82119207d7fe3627f6816521b
Lucas Didur <lucasdidur@hotmail.com>
2014-11-30 00:35:21 -0200

    Alterado modo de como o erro é exibido

--- src/dlp/control/DAO.java
+++ src/dlp/control/DAO.java
@@ -2,6 +2,9 @@ package dlp.control;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,33 +18,32 @@ public class DAO
 
     public DAO()
     {
+        conectar();
+    }
+
+    private static void conectar()
+    {
         try
         {
-            conectar();
-        } catch (Exception ex)
+            String url = "jdbc:postgresql://localhost:5432/gerenciador_financeiro";
+            String usuario = "postgres";
+            String senha = "123456";
+
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection(url, usuario, senha);
+        } catch (ClassNotFoundException | SQLException ex)
         {
             ex.printStackTrace();
             JOptionPane.showMessageDialog(null, "Erro ao fazer conexão com o banco de dados");
-            //System.exit(-1);
         }
-    }
-
-    private static void conectar() throws Exception
-    {
-        String url = "jdbc:postgresql://localhost:5432/gerenciador_financeiro";
-        String usuario = "postgres";
-        String senha = "123456";
-
-        Class.forName("org.postgresql.Driver");
-
-        con = DriverManager.getConnection(url, usuario, senha);
     }
 
     public static Connection getConnection()
     {
         if (con == null)
         {
-            new DAO();
+            conectar();
         }
 
         return con;
