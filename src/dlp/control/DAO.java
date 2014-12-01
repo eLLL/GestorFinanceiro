00340db2d1f575f063e156132952fac52f0df982
Lucas Didur <lucasdidur@hotmail.com>
2014-12-01 16:43:22 -0200

    Atualizado sobre, e criado script de instalacao

--- src/dlp/control/DAO.java
+++ src/dlp/control/DAO.java
@@ -5,6 +5,7 @@ import java.sql.*;
 import java.util.*;
 import java.util.logging.*;
 import javax.swing.*;
+import org.postgresql.util.*;
 
 /**
  *
@@ -19,11 +20,37 @@ public class DAO
     {
         if (conectar())
         {
+            if (!checkTable("Categoria"))
+            {
+                try
+                {
+                    String sql = String.format("CREATE TABLE categoria (id_categoria serial NOT NULL, nome VARCHAR(80) NOT NULL, tipo  VARCHAR(10), cor VARCHAR(20), CONSTRAINT categoria_tipo_check CHECK ((tipo = 'DESPESA') OR (tipo= 'RECEITA')) ); INSERT INTO Categoria VALUES(DEFAULT, 'Padrao Despesa', 'DESPESA', 'VERMELHO'); INSERT INTO Categoria VALUES(DEFAULT, 'Padrao Receita', 'RECEITA', 'VERDE');");
+
+                    PreparedStatement pstmt = con.prepareStatement(sql);
+                    pstmt.executeUpdate();
+                } catch (SQLException ex)
+                {
+                }
+            }
+
+            if (!checkTable("Operacao"))
+            {
+                try
+                {
+                    String sql = String.format("CREATE TABLE operacao (id_operacao serial NOT NULL, id_categoria integer NOT NULL, descricao text, data date DEFAULT CURRENT_DATE, valor numeric(14,2));");
+
+                    PreparedStatement pstmt = con.prepareStatement(sql);
+                    pstmt.executeUpdate();
+                } catch (SQLException ex)
+                {
+                }
+            }
+
             try
             {
                 carregarCategorias();
                 carregarOperacoes();
-                
+
             } catch (SQLException ex)
             {
                 Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -33,18 +60,36 @@ public class DAO
 
     private static boolean conectar()
     {
+        String usuario = "postgres";
+        String senha = "123456";
+
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex)
+        {
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try
+        {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/?user=" + usuario + "&password=" + senha);
+            Statement s = conn.createStatement();
+            s.executeUpdate("CREATE DATABASE gerenciador_financeiro");
+            s.close();
+            conn.close();
+        } catch (Exception e)
+        {
+        }
+
         try
         {
             String url = "jdbc:postgresql://localhost:5432/gerenciador_financeiro";
-            String usuario = "postgres";
-            String senha = "123456";
-
-            Class.forName("org.postgresql.Driver");
 
             con = DriverManager.getConnection(url, usuario, senha);
 
             return true;
-        } catch (ClassNotFoundException | SQLException ex)
+        } catch (SQLException ex)
         {
             ex.printStackTrace();
             JOptionPane.showMessageDialog(null, "Erro ao fazer conexão com o banco de dados");
@@ -60,6 +105,31 @@ public class DAO
         }
 
         return con;
+    }
+
+    public boolean checkTable(String tableName)
+    {
+        DatabaseMetaData dbm = null;
+
+        try
+        {
+            dbm = con.getMetaData();
+            ResultSet tables = dbm.getTables(null, null, tableName, null);
+
+            if (tables.next())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     private void carregarCategorias() throws SQLException
--- src/dlp/view/Sobre.java
+++ src/dlp/view/Sobre.java
@@ -45,7 +45,6 @@ public class Sobre extends javax.swing.JFrame {
         jMenu12 = new javax.swing.JMenu();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -53,6 +52,7 @@ public class Sobre extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
@@ -214,10 +214,6 @@ public class Sobre extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setText("Desenvolvido por:");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel2.setText("- Lucas Padilha  << Esse manja nada");
-        jLabel2.setToolTipText("");
-
         jLabel3.setText("- Anderson Matheus Lemke");
 
         jLabel4.setText("- Lucas Fernando Didur");
@@ -242,38 +238,40 @@ public class Sobre extends javax.swing.JFrame {
             }
         });
 
+        jLabel9.setText("- Lucas Padilha");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addComponent(jLabel2)
                     .addComponent(jLabel3)
                     .addComponent(jLabel4)
                     .addComponent(jLabel5)
                     .addComponent(jLabel6)
                     .addComponent(jLabel7)
-                    .addComponent(jLabel8))
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel9))
                 .addContainerGap(17, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 114, Short.MAX_VALUE)
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel6)
@@ -654,13 +652,13 @@ public class Sobre extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu10;
     private javax.swing.JMenu jMenu11;
